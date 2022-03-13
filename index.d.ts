9609d95711c0264547f53caf597998d97fe3136e
MARCROCK22 <marcos22dev@gmail.com>
2022-03-13 12:01:52 -0400

    types

--- index.d.ts
+++ index.d.ts
@@ -4,8 +4,8 @@ declare namespace GameBattleShip {
         boards: [number[][], number[][]];
         boardsAttacked: [number[][], number[][]];
         turn: 0 | 1;
-        canPlayShips: [[number, number, number, number], [number, number, number, number]];
-        chooseShip(player: 0 | 1, ship: 5 | 4 | 3 | 2, x: 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9, y: 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9, orientation: 'h' | 'v'): void
+        canPlayShips: [number[], number[]];
+        chooseShip(player: 0 | 1, ship: number, x: 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9, y: 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9, orientation: 'h' | 'v'): void
         attack(x: 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9, y: 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9): void
         get alternTurn(): 1 | 0
         checkIfWin(): boolean
