bdb4446ba58b25635f21a5a773b0faabcc95b55d
MARCROCK22 <marcos22dev@gmail.com>
2022-03-15 20:35:23 -0400

    imagina

--- index.d.ts
+++ index.d.ts
@@ -5,8 +5,8 @@ declare namespace GameBattleShip {
         boardsAttacked: [number[][], number[][]];
         turn: 0 | 1;
         canPlayShips: [number[], number[]];
-        chooseShip(player: 0 | 1, ship: number, x:number, y:number, orientation: 'h' | 'v'): void
-        attack(x: number, y:number): void
+        chooseShip(player: 0 | 1, ship: number, x: number, y: number, orientation: 'h' | 'v'): void
+        attack(x: number, y: number): void
         get alternTurn(): 1 | 0
         checkIfWin(): boolean
         get finished(): boolean
